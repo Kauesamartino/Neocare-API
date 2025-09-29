88f3fc813ded5fa8646b050aa3384d3b17d605c1
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-29 16:03:19 -0300

    Initializing project

---
+++ src/main/java/com/braincare/api/BrainCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.braincare.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braincare/api/BrainCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braincare.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
