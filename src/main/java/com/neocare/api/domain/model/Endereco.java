82c7ade941072bfe78c4f2353a1538e5dfc07efa
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-30 09:35:01 -0300

    feat(domain/endereco): implement methods and validations to Endereco

--- src/main/java/com/neocare/api/domain/model/Endereco.java
+++ src/main/java/com/neocare/api/domain/model/Endereco.java
@@ -1,5 +1,9 @@
 package com.neocare.api.domain.model;
 
+import com.neocare.api.domain.exception.ValidacaoDominioException;
+
+import java.util.Locale;
+
 public class Endereco {
 
     private String logradouro;
@@ -16,4 +20,35 @@ public class Endereco {
 
     private String uf;
 
+    public Endereco(String logradouro, String bairro, String cep, String numero, String complemento, String cidade, String uf) {
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        setCep(cep);
+        this.numero = numero;
+        this.complemento = complemento;
+        this.cidade = cidade;
+        setUf(uf);
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+        isCepValido();
+    }
+
+    private void isCepValido() {
+        if(!this.cep.matches("\\d{5}-\\d{3}")){
+            throw new ValidacaoDominioException("CEP inválido, utilize o formato XXXXX-XXX");
+        }
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf.toUpperCase(Locale.ROOT);
+        isUfValido();
+    }
+
+    private void isUfValido() {
+        if (uf.length() != 2) {
+            throw new ValidacaoDominioException("Formato de UF inválido, formato deve ser XX");
+        }
+    }
 }
