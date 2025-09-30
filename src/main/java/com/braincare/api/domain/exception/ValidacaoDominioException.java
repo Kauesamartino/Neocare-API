99d74f89f273d7a4387b6c13746d0b868a8af290
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-30 03:29:18 -0300

    add validacaoDominioException

---
+++ src/main/java/com/braincare/api/domain/exception/ValidacaoDominioException.java
@@ -0,0 +1,7 @@
+package com.braincare.api.domain.exception;
+
+public class ValidacaoDominioException extends RuntimeException {
+    public ValidacaoDominioException(String message) {
+        super(message);
+    }
+}
