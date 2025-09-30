d1394460adafaf8d01e4809bf3ed6ca0364d6cfa
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-30 09:44:53 -0300

    feat(domain/dispositivo): implement methods and validations to Dispositivo

--- src/main/java/com/neocare/api/domain/model/Dispositivo.java
+++ src/main/java/com/neocare/api/domain/model/Dispositivo.java
@@ -1,5 +1,7 @@
 package com.neocare.api.domain.model;
 
+import com.neocare.api.domain.exception.ValidacaoDominioException;
+
 public class Dispositivo {
 
     private Long id;
@@ -10,7 +12,7 @@ public class Dispositivo {
 
     private Integer numeroSerie;
 
-    private Boolean status;
+    private Boolean ativo;
 
     public enum TipoDispositivo {
         ESP32,
@@ -18,4 +20,51 @@ public class Dispositivo {
         SENSOR_CARDIACO
     }
 
+    public Dispositivo(Long usuarioId, TipoDispositivo tipoDispositivo, Integer numeroSerie) {
+        setUsuarioId(usuarioId);
+        setTipoDispositivo(tipoDispositivo);
+        setNumeroSerie(numeroSerie);
+        this.ativo = true;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        if (usuarioId == null) {
+            throw new ValidacaoDominioException("Id do usuário não pode ser nulo");
+        }
+        this.usuarioId = usuarioId;
+    }
+
+    public void setTipoDispositivo(TipoDispositivo tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+        if (tipoDispositivo == null || tipoDispositivo.describeConstable().isEmpty()) {
+            throw new ValidacaoDominioException("Tipo de dispositivo não pode ser nulo ou vazio");
+        }
+    }
+
+    public void setNumeroSerie(Integer numeroSerie) {
+        this.numeroSerie = numeroSerie;
+        if (numeroSerie == null) {
+            throw new ValidacaoDominioException("Número de série não pode ser vazio");
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public TipoDispositivo getTipoDispositivo() {
+        return tipoDispositivo;
+    }
+
+    public Integer getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
 }
