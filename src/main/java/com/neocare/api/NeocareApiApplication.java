848103da03d79ae9ca07d61e08e6e8cef0228e15
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-02 01:42:12 -0300

    fix(properties): rename 'BrainCareApiApplication' to 'NeocareApiApplication'

--- src/main/java/com/neocare/api/NeocareApiApplication.java
+++ src/main/java/com/neocare/api/NeocareApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrainCareApiApplication {
+public class NeocareApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrainCareApiApplication.class, args);
+		SpringApplication.run(NeocareApiApplication.class, args);
 	}
 
 }
