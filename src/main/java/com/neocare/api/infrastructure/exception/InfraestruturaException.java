ddce5ba7bbcde6a86f87b1e56d238e1a7dd34c4d
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-02 00:56:36 -0300

    feat(exception): add InfraestruturaException

---
+++ src/main/java/com/neocare/api/infrastructure/exception/InfraestruturaException.java
@@ -0,0 +1,12 @@
+package com.neocare.api.infrastructure.exception;
+
+public class InfraestruturaException extends RuntimeException {
+    public InfraestruturaException(String message) {
+        super(message);
+    }
+
+    public InfraestruturaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
