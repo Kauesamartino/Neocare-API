472c78fb3bf30fa4331ae5c70013370303055820
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-23 00:51:30 -0300

    feat(config): add DesativarUsuarioUseCase to ControllersConfig and UsuarioUseCasesConfig

--- src/main/java/com/neocare/api/infrastructure/config/ControllersConfig.java
+++ src/main/java/com/neocare/api/infrastructure/config/ControllersConfig.java
@@ -1,9 +1,6 @@
 package com.neocare.api.infrastructure.config;
 
-import com.neocare.api.application.usecase.usuario.CriarUsuarioUseCase;
-import com.neocare.api.application.usecase.usuario.EditarUsuarioUseCase;
-import com.neocare.api.application.usecase.usuario.LocalizarTodosOsUsuariosUseCase;
-import com.neocare.api.application.usecase.usuario.LocalizarUsuarioUseCase;
+import com.neocare.api.application.usecase.usuario.*;
 import com.neocare.api.interfaces.controller.UsuarioController;
 import com.neocare.api.interfaces.controller.UsuarioControllerImpl;
 import org.springframework.context.annotation.Bean;
@@ -17,13 +14,15 @@ public class ControllersConfig {
             CriarUsuarioUseCase criarUsuarioUseCase,
             LocalizarUsuarioUseCase localizarUsuarioUseCase,
             LocalizarTodosOsUsuariosUseCase localizarTodosOsUsuariosUseCase,
-            EditarUsuarioUseCase editarUsuarioUseCase
+            EditarUsuarioUseCase editarUsuarioUseCase,
+            DesativarUsuarioUseCase desativarUsuarioUseCase
     ){
         return new UsuarioControllerImpl(
                 criarUsuarioUseCase,
                 localizarUsuarioUseCase,
                 localizarTodosOsUsuariosUseCase,
-                editarUsuarioUseCase);
+                editarUsuarioUseCase,
+                desativarUsuarioUseCase);
     }
 
 }
--- src/main/java/com/neocare/api/infrastructure/config/UsuarioUseCasesConfig.java
+++ src/main/java/com/neocare/api/infrastructure/config/UsuarioUseCasesConfig.java
@@ -1,15 +1,8 @@
 package com.neocare.api.infrastructure.config;
 
-import com.neocare.api.application.usecase.usuario.CriarUsuarioUseCaseImpl;
-import com.neocare.api.application.usecase.usuario.EditarUsuarioUseCaseImpl;
-import com.neocare.api.application.usecase.usuario.LocalizarUsuarioUseCaseImpl;
-import com.neocare.api.application.usecase.usuario.LocalizarTodosOsUsuariosUseCaseImpl;
+import com.neocare.api.application.usecase.usuario.*;
 
 import com.neocare.api.domain.repository.UsuarioRepository;
-import com.neocare.api.application.usecase.usuario.CriarUsuarioUseCase;
-import com.neocare.api.application.usecase.usuario.EditarUsuarioUseCase;
-import com.neocare.api.application.usecase.usuario.LocalizarTodosOsUsuariosUseCase;
-import com.neocare.api.application.usecase.usuario.LocalizarUsuarioUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,5 +33,10 @@ public class UsuarioUseCasesConfig {
     @Bean
     public EditarUsuarioUseCase editarUsuarioUseCase() {
         return new EditarUsuarioUseCaseImpl(localizarUsuarioUseCase(), usuarioRepository);
+    }
+
+    @Bean
+    public DesativarUsuarioUseCase desativarUsuarioUseCase() {
+        return new DesativarUsuarioUseCaseImpl(usuarioRepository, localizarUsuarioUseCase());
     }
 }
