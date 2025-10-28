366f7e7cdae9f4af63be5ed249c7feefc15bd371
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-28 18:00:32 -0300

    feat(config): add DispositivoRepository bean for dispositivo management

--- src/main/java/com/neocare/api/infrastructure/config/DatabaseConfig.java
+++ src/main/java/com/neocare/api/infrastructure/config/DatabaseConfig.java
@@ -1,7 +1,10 @@
 package com.neocare.api.infrastructure.config;
 
+import com.neocare.api.domain.repository.DispositivoRepository;
 import com.neocare.api.domain.repository.UsuarioRepository;
+import com.neocare.api.infrastructure.persistance.DispositivoRepositoryAdapter;
 import com.neocare.api.infrastructure.persistance.UsuarioRepositoryAdapter;
+import com.neocare.api.infrastructure.repository.JpaDispositivoRepository;
 import com.neocare.api.infrastructure.repository.JpaUsuarioRepository;
 import com.neocare.api.domain.logging.Logger;
 import com.neocare.api.infrastructure.logging.LoggerFactory;
@@ -15,6 +18,12 @@ public class DatabaseConfig {
     public UsuarioRepository clienteRepository(JpaUsuarioRepository jpaUsuarioRepository) {
         final Logger logger = LoggerFactory.getLogger(UsuarioRepositoryAdapter.class);
         return new UsuarioRepositoryAdapter(jpaUsuarioRepository, logger);
+    }
+
+    @Bean
+    public DispositivoRepository dispositivoRepository(JpaDispositivoRepository jpaDispositivoRepository) {
+        final Logger logger = LoggerFactory.getLogger(DispositivoRepositoryAdapter.class);
+        return new DispositivoRepositoryAdapter(jpaDispositivoRepository, logger);
     }
 
 }
