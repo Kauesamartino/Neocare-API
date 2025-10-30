5c0f6704c2525caf013844280597ee92aa61ffe3
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-30 17:19:45 -0300

    feat(medicao): add refactor Medicao class

--- src/main/java/com/neocare/api/domain/model/Medicao.java
+++ src/main/java/com/neocare/api/domain/model/Medicao.java
@@ -6,14 +6,34 @@ import java.time.LocalDateTime;
 
 public class Medicao {
 
-    private Long id;
+    private String nomeUsuario;
 
-    private Long usuarioId;
-
-    private Long dispositivoId;
+    private String tipoDispositivo;
 
     private LocalDateTime dataMedicao;
 
     private TipoMedicao tipoMedicao;
 
+    public Medicao(String nomeUsuario, String tipoDispositivo, TipoMedicao tipoMedicao) {
+        this.nomeUsuario = nomeUsuario;
+        this.tipoDispositivo = tipoDispositivo;
+        this.dataMedicao = LocalDateTime.now();
+        this.tipoMedicao = tipoMedicao;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public void setTipoDispositivo(String tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+    }
+
+    public void setDataMedicao(LocalDateTime dataMedicao) {
+        this.dataMedicao = dataMedicao;
+    }
+
+    public void setTipoMedicao(TipoMedicao tipoMedicao) {
+        this.tipoMedicao = tipoMedicao;
+    }
 }
