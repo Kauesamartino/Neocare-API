bd5c8d87959da85df51aac0fbc6cf98c4b04cfad
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-30 17:28:31 -0300

    feat(medicao): add MedicaoOutDto for output data transfer

---
+++ src/main/java/com/neocare/api/interfaces/dto/output/MedicaoOutDto.java
@@ -0,0 +1,54 @@
+package com.neocare.api.interfaces.dto.output;
+
+import com.neocare.api.domain.enums.TipoMedicao;
+
+import java.time.LocalDateTime;
+
+public class MedicaoOutDto {
+
+    private Long id;
+    private String nomeUsuario;
+    private DispositivoMedicaoOutDto dispositivo;
+    private LocalDateTime dataMedicao;
+    private TipoMedicao tipoMedicao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public DispositivoMedicaoOutDto getDispositivo() {
+        return dispositivo;
+    }
+
+    public void setDispositivo(DispositivoMedicaoOutDto dispositivo) {
+        this.dispositivo = dispositivo;
+    }
+
+    public LocalDateTime getDataMedicao() {
+        return dataMedicao;
+    }
+
+    public void setDataMedicao(LocalDateTime dataMedicao) {
+        this.dataMedicao = dataMedicao;
+    }
+
+    public TipoMedicao getTipoMedicao() {
+        return tipoMedicao;
+    }
+
+    public void setTipoMedicao(TipoMedicao tipoMedicao) {
+        this.tipoMedicao = tipoMedicao;
+    }
+}
