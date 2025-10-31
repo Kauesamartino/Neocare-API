d26dffac6b8cc3b6da60ccc5c30b8684232a66b3
Kaue Samartino <134535906+Kauesamartino@users.noreply.github.com>
2025-10-30 21:44:09 -0300

    feat(repository): add MedicaoEstresseRepository

---
+++ src/main/java/com/neocare/api/domain/repository/MedicaoEstresseRepository.java
@@ -0,0 +1,7 @@
+package com.neocare.api.domain.repository;
+
+import com.neocare.api.domain.model.MedicaoEstresse;
+
+public interface MedicaoEstresseRepository {
+    MedicaoEstresse save(MedicaoEstresse medicaoEstresse);
+}
