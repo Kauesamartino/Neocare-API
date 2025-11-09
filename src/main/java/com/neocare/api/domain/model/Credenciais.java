66348b95332e7f6785e35f2b48308838ef58d3b4
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-09 15:43:32 -0300

    feat(config): add JWT configuration properties and logging levels

--- src/main/java/com/neocare/api/domain/model/Credenciais.java
+++ src/main/java/com/neocare/api/domain/model/Credenciais.java
@@ -1,6 +1,5 @@
 package com.neocare.api.domain.model;
 
-import com.neocare.api.infrastructure.entity.JpaRoleEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
