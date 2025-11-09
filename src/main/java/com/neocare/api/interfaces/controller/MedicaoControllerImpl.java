122e865334d0fc939971acf0a9ec3c633e3e168c
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-09 15:49:32 -0300

    feat(controller): update MedicaoControllerImpl to remove unused import and add class documentation

--- src/main/java/com/neocare/api/interfaces/controller/MedicaoControllerImpl.java
+++ src/main/java/com/neocare/api/interfaces/controller/MedicaoControllerImpl.java
@@ -3,7 +3,6 @@ package com.neocare.api.interfaces.controller;
 import com.neocare.api.application.usecase.dispositivo.LocalizarDispositivoUseCase;
 import com.neocare.api.application.usecase.medicao.estresse.RegistrarMedicaoEstresseUseCase;
 import com.neocare.api.application.usecase.usuario.LocalizarUsuarioPorIdUseCase;
-import com.neocare.api.application.usecase.usuario.LocalizarUsuarioUseCase;
 import com.neocare.api.domain.model.Dispositivo;
 import com.neocare.api.domain.model.MedicaoEstresse;
 import com.neocare.api.domain.model.Usuario;
@@ -11,7 +10,10 @@ import com.neocare.api.interfaces.dto.input.MedicaoEstresseInDto;
 import com.neocare.api.interfaces.dto.output.MedicaoEstresseOutDto;
 import com.neocare.api.interfaces.mapper.MedicaoEstresseMapper;
 
-
+/**
+ * Implementação do controller de Usuario sem acoplamento com frameworks externos.
+ * Segue os princípios da Clean Architecture, atuando como um adaptador entre os casos de uso e a interface externa.
+ */
 public final class MedicaoControllerImpl implements  MedicaoController {
 
     private final RegistrarMedicaoEstresseUseCase registrarMedicaoEstresse;
