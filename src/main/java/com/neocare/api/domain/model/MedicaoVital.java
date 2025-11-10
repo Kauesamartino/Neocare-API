e2cef0141997af153ab6a38832ebc242b7fa9bec
Kauesamartino <kaue_samartino@hotmail.com>
2025-11-10 05:50:36 -0300

    feat(medicao): add properties and constructor to MedicaoVital for vital measurements

--- src/main/java/com/neocare/api/domain/model/MedicaoVital.java
+++ src/main/java/com/neocare/api/domain/model/MedicaoVital.java
@@ -2,6 +2,8 @@ package com.neocare.api.domain.model;
 
 import com.neocare.api.domain.enums.TipoMedicao;
 
+import java.time.LocalDateTime;
+
 
 public class MedicaoVital extends Medicao {
 
@@ -19,5 +21,29 @@ public class MedicaoVital extends Medicao {
         this.oxigenacaoSangue = oxigenacaoSangue;
         this.pressaoSistolica = pressaoSistolica;
         this.pressaoDiastolica = pressaoDiastolica;
+    }
+
+    public MedicaoVital(Long id, Long idUsuario, Long idDispositivo, LocalDateTime dataMedicao, TipoMedicao tipoMedicao, Integer batimentosPorMinuto, Double oxigenacaoSangue, Integer pressaoSistolica, Integer pressaoDiastolica) {
+        super(id, idUsuario, idDispositivo, dataMedicao, tipoMedicao);
+        this.batimentosPorMinuto = batimentosPorMinuto;
+        this.oxigenacaoSangue = oxigenacaoSangue;
+        this.pressaoSistolica = pressaoSistolica;
+        this.pressaoDiastolica = pressaoDiastolica;
+    }
+
+    public Integer getBatimentosPorMinuto() {
+        return batimentosPorMinuto;
+    }
+
+    public Double getOxigenacaoSangue() {
+        return oxigenacaoSangue;
+    }
+
+    public Integer getPressaoSistolica() {
+        return pressaoSistolica;
+    }
+
+    public Integer getPressaoDiastolica() {
+        return pressaoDiastolica;
     }
 }
